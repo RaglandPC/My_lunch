141185c91b3fb771ba911debc38318098c5e7892
Ragland <jakearagland@gmail.com>
2018-04-25 13:09:19 -0500

    cant figure out the mysql through cmd

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rice burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Salmon burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Slopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Slugburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steamed cheeseburger', true);
